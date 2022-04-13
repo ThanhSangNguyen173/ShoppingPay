d497995a91722dabfe6266299e72b84b1441a148
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-04-13 14:06:51 +0700

    final edit

--- app/src/main/java/com/example/shoppingpay/views/loginregister/IntroPreLogin.java
+++ app/src/main/java/com/example/shoppingpay/views/loginregister/IntroPreLogin.java
@@ -22,24 +22,22 @@ public class IntroPreLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.getSupportActionBar().hide();
-        setContentView(R.layout.fragment_product_detail);
-//
-//        img_intro = findViewById(R.id.img_intro);
-//        zoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
-//        img_intro = findViewById(R.id.img_intro);
-//        img_intro.startAnimation(zoom);
-//
-//        Handler h = new Handler();
-//        h.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                Intent intro = new Intent(getApplicationContext(),LoginRegisterActivity.class);
-//                startActivity(intro);
-//                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
-//                finish();
-//            }
-//        }, 4000);
-//
-//    }
+        setContentView(R.layout.activity_intro);
+        img_intro = findViewById(R.id.img_intro);
+        zoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
+        img_intro = findViewById(R.id.img_intro);
+        img_intro.startAnimation(zoom);
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intro = new Intent(getApplicationContext(),LoginRegisterActivity.class);
+                startActivity(intro);
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_right);
+                finish();
+            }
+        }, 4000);
+
     }
 }
