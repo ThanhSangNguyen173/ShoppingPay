2e2e9e97ec1d63e009b1b202d1c03707a610c7a7
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-04-18 11:08:03 +0700

    edit class file name

--- app/src/main/java/com/example/shoppingpay/views/LoadingDialog.java
+++ app/src/main/java/com/example/shoppingpay/views/LoadingDialog.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
