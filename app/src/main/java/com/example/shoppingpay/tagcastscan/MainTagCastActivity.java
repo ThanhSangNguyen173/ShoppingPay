f97c7d0c893423cf4a253112ee2d925ca4e3aa6f
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-04-19 10:14:36 +0700

    edit layout tagcast scan add change table

--- app/src/main/java/com/example/shoppingpay/tagcastscan/MainTagCastActivity.java
+++ app/src/main/java/com/example/shoppingpay/tagcastscan/MainTagCastActivity.java
@@ -1,7 +1,5 @@
 package com.example.shoppingpay.tagcastscan;
 
-import static com.example.shoppingpay.R.drawable.scanfail;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,7 +8,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -20,7 +20,10 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.shoppingpay.R;
-import com.example.shoppingpay.views.activity.choosetable.UsingTableActivity;
+import com.example.shoppingpay.views.activity.MainShoppingActivity;
+import com.example.shoppingpay.views.activity.choosetable.ChooseTableActivity;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 import java.util.Map;
@@ -39,7 +42,10 @@ public class MainTagCastActivity extends AppCompatActivity implements ActivityCo
     private String serial, pickserial, tablenumber;
     private Map<String,String> map;
     public int mErrorDialogType = ErrorFragment.TYPE_NO;
-    ImageButton btn_scan;
+    DatabaseReference mData;
+    Button btn_scan, btn_changeTable, btn_goToMenu;
+    TextView txt_tablenumber;
+    ImageView img_table;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,22 +53,116 @@ public class MainTagCastActivity extends AppCompatActivity implements ActivityCo
         super.getSupportActionBar().hide();
         setContentView(R.layout.activity_tagcast_main);
 
+        mData = FirebaseDatabase.getInstance().getReference();
         Bundle bundle = getIntent().getExtras();
         pickserial = bundle.getString("seri");
         tablenumber = bundle.getString("table");
 
         anhxa();
         TagCastScan();
+        tableNumber();
+    }
+
+    private void tableNumber() {
+        if (tablenumber.equals("table1")) {
+            txt_tablenumber.setText("Table 1");
+        }
+        if (tablenumber.equals("table2")) {
+            txt_tablenumber.setText("Table 2");
+        }
+        if (tablenumber.equals("table3")) {
+            txt_tablenumber.setText("Table 3");
+        }
+        if (tablenumber.equals("table4")) {
+            txt_tablenumber.setText("Table 4");
+        }
+        if (tablenumber.equals("table5")) {
+            txt_tablenumber.setText("Table 5");
+        }
+        if (tablenumber.equals("table6")) {
+            txt_tablenumber.setText("Table 6");
+        }
+        if (tablenumber.equals("table21")) {
+            txt_tablenumber.setText("Table 1 Floor 2");
+        }
+        if (tablenumber.equals("table22")) {
+            txt_tablenumber.setText("Table 2 Floor 2");
+        }
+        if (tablenumber.equals("table23")) {
+            txt_tablenumber.setText("Table 3 Floor 2");
+        }
+        if (tablenumber.equals("table24")) {
+            txt_tablenumber.setText("Table 4 Floor 2");
+        }
     }
 
     private void anhxa() {
         btn_scan = findViewById(R.id.btn_scan);
         btn_scan.setOnClickListener(this::OnClick);
+
+        btn_changeTable = findViewById(R.id.btn_changetable);
+        btn_changeTable.setOnClickListener(this::OnClick);
+
+        btn_goToMenu = findViewById(R.id.btn_gotomenu);
+        btn_goToMenu.setOnClickListener(this::OnClick);
+
+        txt_tablenumber = findViewById(R.id.txt_tablenumber);
+        img_table = findViewById(R.id.img_table);
     }
 
     private void OnClick(View view) {
+        final Context context = getApplicationContext();
         switch (view.getId()){
             case R.id.btn_scan: scanTagCastPaper();
+                break;
+            case R.id.btn_changetable:
+                changeTable();
+                Intent intent2 = new Intent(MainTagCastActivity.this, ChooseTableActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.btn_gotomenu:
+                Intent intent = new Intent(context, MainShoppingActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("seri",serial);
+                bundle.putString("table",tablenumber);
+                intent.putExtras(bundle);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    private void changeTable() {
+        switch (tablenumber){
+            case "table1":
+                mData.child("TB1").setValue("t");
+                break;
+            case "table2":
+                mData.child("TB2").setValue("t");
+                break;
+            case "table3":
+                mData.child("TB3").setValue("t");
+                break;
+            case "table4":
+                mData.child("TB4").setValue("t");
+                break;
+            case "table5":
+                mData.child("TB5").setValue("t");
+                break;
+            case "table6":
+                mData.child("TB6").setValue("t");
+                break;
+            case "table21":
+                mData.child("TB21").setValue("t");
+                break;
+            case "table22":
+                mData.child("TB22").setValue("t");
+                break;
+            case "table23":
+                mData.child("TB23").setValue("t");
+                break;
+            case "table24":
+                mData.child("TB24").setValue("t");
+                break;
         }
     }
 
@@ -82,18 +182,17 @@ public class MainTagCastActivity extends AppCompatActivity implements ActivityCo
                 loadingDialog.dismissDialog();
                 if(flgBeacon){
                     if(pickserial.equals(serial)){
-                        Intent intent = new Intent(context, UsingTableActivity.class);
-                        Bundle bundle = new Bundle();
-                        bundle.putString("seri",serial);
-                        bundle.putString("table",tablenumber);
-                        intent.putExtras(bundle);
-                        startActivity(intent);
+                        btn_goToMenu.setVisibility(View.VISIBLE);
+                        btn_scan.setEnabled(false);
+                        btn_scan.setText("Scan Sucesfully");
+                        img_table.setBackground(getDrawable(R.drawable.using_table));
                     }else {
                         Toast.makeText(context, "Vui lòng nhận đúng bàn đã chọn", Toast.LENGTH_SHORT).show();
                         btn_scan.setEnabled(true);
+                        btn_scan.setText("Try scan agian");
                     }
                 }else{
-                    btn_scan.setBackground(getDrawable(scanfail));
+                    btn_scan.setText("Try scan agian");
                     btn_scan.setEnabled(true);
                     Toast.makeText(context, "Scan fail, please try again!", Toast.LENGTH_SHORT).show();
                 }
