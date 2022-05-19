9c830158077f1a1538f2b0ca62cfbfc5451148bf
GitRepo <nguyenduchung.lhu@gmail.com>
2022-05-19 14:35:54 +0700

    [Call Api] sua giao dien va logic resgister

--- app/src/main/java/com/example/shoppingpay/views/activity/loginregister/RegisterActivity.java
+++ app/src/main/java/com/example/shoppingpay/views/activity/loginregister/RegisterActivity.java
@@ -3,10 +3,17 @@ package com.example.shoppingpay.views.activity.loginregister;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.shoppingpay.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText edtfull_name, edtusername,edtpassword,edtemail,edtDOB,edtCfpassword;
+    Button btnResigen, btnhuy;
+    String  full_name, username,password,email,DOB;
 
 
     @Override
@@ -14,55 +21,27 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        anhXa();
 
+        btnResigen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-//        Dialog dialog = new Dialog(LoginRegisterActivity.this);
-//        dialog.setTitle("ĐĂNG KÝ TÀI KHOẢN");
-//        dialog.setCancelable(false);
-//        dialog.setContentView(R.layout.dky_page);
-//        dky2 = dialog.findViewById(R.id.dky2);
-//        ho = dialog.findViewById(R.id.editTextHo);
-//        ten = dialog.findViewById(R.id.editTextTen);
-//        dob = dialog.findViewById(R.id.editTextDOB);
-//        pass = dialog.findViewById(R.id.editTextPass);
-//        copass = dialog.findViewById(R.id.editTextCoPass);
-//        checkBox = dialog.findViewById(R.id.checkBox);
-//        nam = dialog.findViewById(R.id.nam);
-//        nu = dialog.findViewById(R.id.nu);
-//        id = dialog.findViewById(R.id.editTextID);
-//        huydk = dialog.findViewById(R.id.huydk);
-//
-//        dky2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                String dk1 = ho.getText().toString().trim();
-//                String dk2 = ten.getText().toString().trim();
-//                String dk3 = dob.getText().toString().trim();
-//                String dk4 = copass.getText().toString().trim();
-//                Boolean b = !checkBox.isChecked();
-//
-//                taikhoan = id.getText().toString().trim();
-//                matkhau = pass.getText().toString().trim();
-//
-//                if (dk1.isEmpty() || dk2.isEmpty() || dk3.isEmpty() || dk4.isEmpty() || taikhoan.isEmpty() ||matkhau.isEmpty() || b ) {
-//                    showToast(0);
-//                }else if (!matkhau.equals(dk4)){
-//                    showToast(6);
-//                }else {
-//                    tk.setText(taikhoan);
-//                    mk.setText(matkhau);
-//                    dialog.cancel();
-//                    showToast(1);
-//                }
-//            }
-//        });
-//        huydk.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                dialog.cancel();
-//            }
-//        });
-//        dialog.show();
+            }
+        });
+
+    }
+
+    private void anhXa() {
+        edtfull_name = findViewById(R.id.editTextFullname);
+        edtDOB=findViewById(R.id.editTextDOB);
+        edtemail=findViewById(R.id.editTextEmail);
+        edtusername=findViewById(R.id.editTextUsername);
+        edtpassword=findViewById(R.id.editTextPass);
+        edtpassword=findViewById(R.id.editTextCofPass);
+
+        btnResigen=findViewById(R.id.btn_register);
+        btnhuy=findViewById(R.id.btnhuy);
+
     }
 }
