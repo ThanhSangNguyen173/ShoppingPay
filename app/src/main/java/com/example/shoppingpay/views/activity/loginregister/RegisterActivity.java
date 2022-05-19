ff2650986b02e708b35a6d11259698da354f426a
GitRepo <nguyenduchung.lhu@gmail.com>
2022-05-19 14:01:36 +0700

    [Call API] khong co thay doi

--- app/src/main/java/com/example/shoppingpay/views/activity/loginregister/RegisterActivity.java
+++ app/src/main/java/com/example/shoppingpay/views/activity/loginregister/RegisterActivity.java
@@ -8,6 +8,7 @@ import com.example.shoppingpay.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
