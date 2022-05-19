7ef5f0e8220c86a52179940beb614b27334b2764
GitRepo <nguyenduchung.lhu@gmail.com>
2022-05-19 13:09:49 +0700

    [CAll API] call api login success

--- app/src/main/java/com/example/shoppingpay/views/activity/loginregister/apiset.java
+++ app/src/main/java/com/example/shoppingpay/views/activity/loginregister/apiset.java
@@ -1,2 +1,15 @@
-package com.example.shoppingpay.views.activity.loginregister;public interface apiset {
+package com.example.shoppingpay.views.activity.loginregister;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface apiset {
+
+    @FormUrlEncoded
+    @POST("login")
+    Call <responsemodel> verifyuser( @Field("username")String username,
+                                     @Field("password")String password);
+
 }
--- app/src/main/java/com/example/shoppingpay/views/activity/loginregister/responsemodel.java
+++ app/src/main/java/com/example/shoppingpay/views/activity/loginregister/responsemodel.java
@@ -1,2 +1,21 @@
-package com.example.shoppingpay.views.activity.loginregister;public class responsemodel {
+package com.example.shoppingpay.views.activity.loginregister;
+
+public class responsemodel {
+    Userlogin userlogin;
+    String message;
+
+    public responsemodel(String message) {
+        this.message = message;
+    }
+
+    public responsemodel() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
